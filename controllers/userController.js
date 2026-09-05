816dd9b2968f1065551e326483949d7622fb1b89
Gagan <vishwakarmagagan24@gmail.com>
2026-09-05 11:09:32 +0530

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,52 @@
-let db = require("../db/connectDB")
+let bcrypt = require("bcrypt");
 let userModel = require("../models/userModel")
 let register = async (req, res) => {
+    try {
+        let { name, email, password, state, mobilenumber } = req.body;
+        if (!name || !email || !password || !state || !mobilenumber) {
+            return res.status(404).json({ success: false, message: "pls provide all the details" })
+        }
 
-    let { name, email, password, state, mobilenumber } = req.body;
-    if (!name || !email || !password || !state || !mobilenumber) {
-        return res.status(404).json({ success: false, message: "pls provide all the details" })
+        let olduser = await userModel.findOne({ email });
+        if (olduser) {
+            return res.status(404).json({ success: false, message: "email already exist" })
+        }
+        let hashpassword = await bcrypt.hash(password, 10);
+        //$2b$10$I5iyKNh.dy3Jfq.0.uBdVOo1dhZ7XEoXkOdK.2Oumkbc6a663xB4u
+        //Ga@8gggh
+        let newuser = await userModel.insertOne({ name, email, password: hashpassword, state, mobilenumber });
+
+        // let newuser = await userModel.create({ name, email, password });
+        // await newuser.save();
+
+        res.status(200).json({ success: true, message: "registration done", data: newuser })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
     }
-
-    let olduser = await userModel.findOne({ email });
-    if (olduser) {
-        return res.status(404).json({ success: false, message: "email already exist" })
-    }
-    let newuser = await userModel.insertOne({ name, email, password, state, mobilenumber })
-
-    // let newuser = await userModel.create({ name, email, password })
-    // await newuser.save();
-
-    res.status(200).json({ success: true, message: "registration done", data: newuser })
 }
 
+// 1111 = jvfk == compare
 
-let login = (req, res) => {
-    res.send("login done")
+let login = async (req, res) => {
+    try {
+        let { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(404).json({ success: false, message: "pls provide all the details" })
+        }
+        let user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ success: false, message: "user not found" })
+        }
+        let isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(404).json({ success: false, message: "pls provide valid credentials" })
+        }
+
+        res.status(200).json({ sucess: true, message: "login successfull" })
+
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
 }
 
 let logout = (req, res) => {
